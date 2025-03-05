9ac373b9f95383f7cc187aabc1756216cbbb0fe7
Sphynx-01 <sphynx.sphynx@mail.ru>
2025-03-05 15:19:17 +0300

    Update lessons.py

--- Modules/lessons.py
+++ Modules/lessons.py
@@ -1,1 +1,13 @@
+class GamifiedLessons:
+    def __init__(self):
+        # Инициализация геймифицированных уроков
+        self.lessons = []
+
+    def add_lesson(self, lesson):
+        # Метод для добавления геймифицированного урока
+        self.lessons.append(lesson)
+
+    def get_lessons(self):
+        # Метод для получения списка геймифицированных уроков
+        return self.lessons
 
