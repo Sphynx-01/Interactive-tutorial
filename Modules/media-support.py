ae5f102b46d1277fa2e9a16ad1687ea33366fa46
Sphynx-01 <sphynx.sphynx@mail.ru>
2025-03-05 15:07:40 +0300

    Update media-support.py

--- Modules/media-support.py
+++ Modules/media-support.py
@@ -1,1 +1,13 @@
+class MediaSupport:
+    def __init__(self):
+        # Инициализация поддержки медиа
+        pass
+
+    def load_media(self, media_path):
+        # Метод для загрузки медиафайлов
+        pass
+
+    def play_media(self, media):
+        # Метод для воспроизведения медиа
+        pass
 
